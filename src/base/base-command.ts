60a7a0c2a6459839e8f4130f4fbbcdd93a51f8ed
Kbd Overlord <kbd.overlord@gmail.com>
2021-12-09 09:34:16 +0100

    feat: implement pattern-based matching in find-pairs

--- src/base/base-command.ts
+++ src/base/base-command.ts
@@ -4,11 +4,31 @@ import { Input, OutputArgs, OutputFlags } from "@oclif/parser";
 import * as signal from "signale";
 import chalk = require("chalk");
 import CFonts = require("cfonts");
+import * as fs from "fs";
+import { ImageParser } from "../helper/image-parser";
+import cliUx from "cli-ux";
 
 export default abstract class BaseCommand extends Command {
   static flags = {
     help: flags.help({ char: "h" }),
+    export: flags.boolean({
+      description: "Enable export",
+      default: false,
+    }),
+    exportAs: flags.string({
+      description: "Set export format. Currently supported: [json]",
+      options: ["json"],
+      dependsOn: ["export"],
+    }),
   };
+  static args = [
+    {
+      name: "path",
+      required: true,
+      description: "Path to file or directory",
+    },
+  ];
+
   protected parsedArgs?: OutputArgs;
   protected parsedFlags?: OutputFlags<typeof BaseCommand.flags>;
   protected sig!: signal.Signale<signal.DefaultMethods>;
@@ -27,25 +47,94 @@ export default abstract class BaseCommand extends Command {
     );
     this.parsedArgs = args;
     this.parsedFlags = flags;
+    this.welcome();
   }
 
   async catch(err: any) {
-    if (err.oclif.exit === 0) {
-      return super.catch(err);
+    return super.catch(err);
+  }
+
+  async validateDir(dir: string): Promise<boolean> {
+    this.sig.start(`Preparing to scan directory at ${dir}`);
+    this.sig.await("Checking directory...");
+    if (fs.existsSync(dir)) {
+      this.sig.success("Directory exists");
+      const parser = await this.initializeParser(dir);
+      const files = this.prepFileList(parser);
+      let allFilesAreJPEG = false;
+      files.forEach((element) => {
+        if (element.toLocaleLowerCase().endsWith("jpg")) {
+          allFilesAreJPEG = true;
+        } else {
+          allFilesAreJPEG = false;
+        }
+      });
+      parser.destroy();
+      if (allFilesAreJPEG) {
+        return true;
+      } else {
+        throw new Error(
+          "Directory has files other than JPEG images. Clean up and try again"
+        );
+      }
     } else {
-      this.sig.error("Oooops...ERROR!");
-      return super.catch(err);
+      throw new Error("Directory does not exist");
     }
   }
+
+  async initializeParser(dir: string): Promise<ImageParser> {
+    this.sig.await("Initializing parser...");
+    const parser = new ImageParser(dir);
+    await parser.init();
+    this.sig.success("Parser initialized");
+    return parser;
+  }
+
+  prepFileList(parser: ImageParser): string[] {
+    this.sig.await("Reading files...");
+    const files = parser.files;
+    if (files.length >= 1) {
+      this.sig.success("File list ready");
+    } else {
+      throw new Error("Directory is empty");
+    }
+    return files;
+  }
+
+  exitWithSuccess(message: string): void {
+    this.sig.complete(`${message}`);
+    this.showNewLine();
+  }
+
+  saveAsJSON(subject: string, path: string, toSave: any): void {
+    this.sig.await(`Exporting ${subject}...`);
+    let data = JSON.stringify(toSave);
+    fs.writeFileSync(path, data);
+    this.sig.success(`${subject} exported successfully. File: ${path}`);
+  }
+
+  abstract printFlags(flags: any): void;
 
   //   Helper
   protected showSeparator(): void {
     this.log("-----------------------------------------------------------");
   }
 
+  protected showNewLine(): void {
+    this.log("\n");
+  }
+
+  getNewProgressBar(subject: string): any {
+    return cliUx.progress({
+      format: `PROGRESS | {bar} | {value}/{total} ${subject}`,
+      barCompleteChar: "\u2588",
+      barIncompleteChar: "\u2591",
+    });
+  }
+
   protected welcome(): void {
     console.clear();
-    this.log("\n");
+    this.showNewLine();
     CFonts.say("exif-util", {
       font: "simple",
       align: "left",
@@ -54,11 +143,11 @@ export default abstract class BaseCommand extends Command {
       background: "transparent",
       env: "node",
     });
-    this.log("\n");
+    this.showNewLine();
     this.log(
       chalk.bgGreen.black(`  exif-util version ${this.config.version}   `)
     );
-    this.log("\n");
+    this.showNewLine();
     this.showSeparator();
   }
 }
--- src/commands/find-pairs.ts
+++ src/commands/find-pairs.ts
@@ -1,3 +1,4 @@
+import { OutputFlags } from "@oclif/parser";
 import { flags } from "@oclif/command";
 import BaseCommand from "../base/base-command";
 
@@ -6,34 +7,189 @@ export default class FindPairs extends BaseCommand {
 
   static flags = {
     ...BaseCommand.flags,
-    byAltitude: flags.string({
-      description: "Margin for EXIF tag GPSAltitude",
-    }),
-    byLongitude: flags.string({
-      description: "Margin for EXIF tag GPSLongitude",
-    }),
-    byLatitude: flags.string({
-      description: "Margin for EXIF tag GPSLatitude",
-    }),
-    byDateTime: flags.string({
-      description:
-        "Margin for EXIF tag DateTimeOriginal. Provide UNIX timestamp in seconds",
+    matchingMode: flags.string({
+      description: "Choose method for finding pairs",
+      options: ["pattern"],
       required: true,
+      dependsOn: ["startsWith"],
     }),
+    startsWith: flags.string({
+      description: "Specify which image comes first",
+      options: ["RGB", "Radiometric"],
+      dependsOn: ["matchingMode", "patternRGB", "patternRadiometric"],
+    }),
+    patternRGB: flags.string({
+      description: "Pattern to look for in a RGB image",
+      dependsOn: ["matchingMode", "startsWith", "patternRadiometric"],
+    }),
+    patternRadiometric: flags.string({
+      description: "Pattern to look for in a Radiometric image",
+      dependsOn: ["matchingMode", "startsWith", "patternRGB"],
+    }),
+    // dateTimeWithin: flags.string({
+    //   description: "Margin for EXIF tag DateTimeOriginal. In seconds",
+    // }),
+    // altitudeWithin: flags.string({
+    //   description: "Margin for EXIF tag GPSAltitude",
+    // }),
+    // longitudeWithin: flags.string({
+    //   description: "Margin for EXIF tag GPSLongitude. In degrees",
+    // }),
+    // latitudeWithin: flags.string({
+    //   description: "Margin for EXIF tag GPSLatitude. In degrees",
+    // }),
+    export: {
+      ...BaseCommand.flags.export,
+      description: "Enable export of image pairs list",
+    },
   };
 
   static args = [
     {
-      name: "directory",
-      required: true,
-      description: "Directory to scan for pairs",
+      ...BaseCommand.args[0],
+      description: "Path to directory to look for pairs",
     },
   ];
 
   async run() {
-    this.welcome();
-    const { args, flags } = this.parse(FindPairs);
-    console.log(args);
-    console.log(flags);
+    if (this.parsedArgs && this.parsedFlags) {
+      // Read flags and args
+      const flags = this.parsedFlags as OutputFlags<typeof FindPairs.flags>;
+      const args = this.parsedArgs;
+
+      // Notify on set flags
+      this.printFlags(flags);
+
+      if (
+        flags.patternRGB &&
+        flags.patternRadiometric &&
+        flags.startsWith &&
+        args.path
+      ) {
+        await this.findByPattern(
+          flags.patternRGB,
+          flags.patternRadiometric,
+          flags.startsWith,
+          args.path,
+          flags
+        );
+      }
+    }
+  }
+
+  printFlags(flags: any): void {
+    if (flags.export) {
+      this.sig.info("--export is enabled");
+
+      if (flags.exportAs) {
+        this.sig.info(`--exportAs set to ${flags.exportAs}`);
+      } else {
+        this.sig.warn(
+          `Export format not specified. Export will not be executed`
+        );
+        this.sig.info(`Use --exportAs flag to specify format`);
+      }
+    }
+
+    if (flags.matchingMode) {
+      this.sig.info(`--matchingMode set to "${flags.matchingMode}"`);
+
+      if (flags.startsWith) {
+        this.sig.info(`--startsWith set to "${flags.startsWith}"`);
+      }
+
+      if (flags.patternRGB) {
+        this.sig.info(`--patternRGB set to "${flags.patternRGB}"`);
+      }
+
+      if (flags.patternRadiometric) {
+        this.sig.info(
+          `--patternRadiometric set to "${flags.patternRadiometric}"`
+        );
+      }
+    }
+  }
+
+  async findByPattern(
+    patternRGB: string,
+    patternRadiometric: string,
+    startsWith: "RGB" | "Radiometric",
+    directory: string,
+    flags: any
+  ): Promise<void> {
+    const isDirectoryValid = await this.validateDir(directory);
+
+    if (isDirectoryValid) {
+      const parser = await this.initializeParser(directory);
+      const files = this.prepFileList(parser);
+      parser.destroy();
+      const isPatternValid = this.checkDirectoryPattern(
+        patternRGB,
+        patternRadiometric,
+        startsWith,
+        files
+      );
+      if (isPatternValid) {
+        this.sig.success("Pattern matched. Printing files in order now");
+        this.showSeparator();
+        this.showNewLine();
+        console.log(files);
+        this.showNewLine();
+        this.showSeparator();
+        if (flags.export && flags.exportAs) {
+          switch (flags.exportAs) {
+            case "json":
+              const path = directory + "/" + "pairs.json";
+              this.saveAsJSON("Pairs", path, files);
+              break;
+
+            default:
+              break;
+          }
+        }
+        this.exitWithSuccess("Found pairs successfully. Exiting now");
+      } else {
+        throw new Error("Pattern did not match. Try again");
+      }
+    }
+  }
+
+  checkDirectoryPattern(
+    patternRGB: string,
+    patternRadiometric: string,
+    startsWith: "RGB" | "Radiometric",
+    files: string[]
+  ): boolean {
+    let valid = false;
+    this.sig.watch("Checking directory pattern...");
+    const progressBar = this.getNewProgressBar("Files");
+    this.showSeparator();
+    this.showNewLine();
+    progressBar.start(files.length / 2, 0);
+    for (let index = 0; index < files.length; index += 2) {
+      let patternMatch1 = false;
+      let patternMatch2 = false;
+
+      switch (startsWith) {
+        case "Radiometric":
+          patternMatch1 = files[index].endsWith(patternRadiometric);
+          patternMatch2 = files[index + 1].endsWith(patternRGB);
+          break;
+
+        case "RGB":
+          patternMatch1 = files[index].endsWith(patternRGB);
+          patternMatch2 = files[index + 1].endsWith(patternRadiometric);
+          break;
+
+        default:
+          break;
+      }
+      valid = patternMatch1 && patternMatch2;
+      progressBar.increment();
+    }
+    progressBar.stop();
+    this.showNewLine();
+    this.showSeparator();
+    return valid;
   }
 }
